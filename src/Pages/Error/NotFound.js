be137eb44d224f36141219cdbcf26eba894f4346
Max <gmaxgomes@gmail.com>
2021-06-04 20:26:20 -0300

    routes + header

---
+++ src/Pages/Error/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      Ops, nada por aqui...
+    </div>
+  )
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
+  ,
   document.getElementById('root')
 );
 
